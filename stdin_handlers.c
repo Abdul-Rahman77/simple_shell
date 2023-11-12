6db80cb213f025717902bef6d6883bbae9ce2af6
anniedevkiller <erisannieannie@gmail.com>
2023-11-12 07:07:54 -0800

    still in betty stdin_handlers.c

--- stdin_handlers.c
+++ stdin_handlers.c
@@ -28,27 +28,26 @@ ssize_t _getline(char **lineptr, size_t *n, FILE *stream)
 	size_t position = 0;
 	ssize_t counter = 0;
 
-      if (lineptr == NULL || n == NULL || stream == NULL)
-              return (-1);
-        if (*lineptr == NULL || *n == 0)
-        {
-                *n = 120;
-                *lineptr = (char *)malloc(sizeof(char) * (*n));
-                if (*lineptr == NULL)
-                        return (-1);
-        }
-        while ((character = _getchar()) != EOF)
-        {
-                if ((position + 1) >= *n)
-                {
-                        size_t size = (*n * 2);
-                        char *line = realloc(*lineptr, size);
-
-                        if (line == NULL)
-                        {
-                                return (-1);
-                        }
-                *lineptr = line;
+	if (lineptr == NULL || n == NULL || stream == NULL)
+		return (-1);
+	if (*lineptr == NULL || *n == 0)
+	{
+		*n = 120;
+		*lineptr = (char *)malloc(sizeof(char) * (*n));
+		if (*lineptr == NULL)
+			return (-1);
+	}
+	while ((character = _getchar()) != EOF)
+	{
+		if ((position + 1) >= *n)
+		{
+			size_t size = (*n * 2);
+			char *line = realloc(*lineptr, size);
+			if (line == NULL)
+			{
+				return (-1);
+			}
+			*lineptr = line;
                 *n = size;
                 }
                 (*lineptr)[position] = (char)character;
